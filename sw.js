1d62e610d8944bda59ac4f0b419f12a7726e1ade
PhysiqueRUSH <physiquerush@gmail.com>
2026-02-17 12:09:26 +0100

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-const CACHE='physioflow-patient-v4';
-const ASSETS=['./index.html','./manifest.json'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>{
-  if(e.request.url.includes('firebasedatabase.app')||e.request.url.includes('googleapis.com')||e.request.url.includes('gstatic.com'))return;
-  e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
-});
